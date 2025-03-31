87f94616002774665279151ae4efdac67e4c2d0d
me-key-0 <me.key.as.24@gmail.com>
2025-03-31 21:00:41 +0300

    search controllers

--- server.js
+++ server.js
@@ -9,4 +9,7 @@ const server = app.listen(port, () => {
 
 connectDb();
 
+app.use(express.json());
 app.use("/api/v1/search", require("./routes/v1/search_routes"));
+app.use("/api/v1/auth", require("./routes/v1/auth_routes"));
+app.use("/api/v1/user", require("./routes/v1/user_routes"));
